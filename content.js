0bd9016cba836cb0a418ae81ca57fddb5ac9b4f9
Dan Zinkevich <dan@sludgefree.net>
2018-09-28 02:23:39 -0400

    Refactor for better granularity.
    For example, now user can still select just headlines while having Twitter names switched globally.

--- content.js
+++ content.js
@@ -6,25 +6,37 @@ chrome.storage.sync.get({
   links: false,
   twitter: true,
 }, function(items) {
+  var places = [];
+
+  // Different levels of how much replacement to do
   if (items.aggressive) {
-    replaceInType('*', items.twitter);
+    places = places.concat('*');
   } else {
     if (items.headlines) {
-      replaceInType('H1', items.twitter);
+      places = places.concat('H1');
     }
     if (items.links) {
-      replaceInTypes(['A', 'STRONG', 'SPAN', 'B'], items.twitter);
+      places = places.concat(['A', 'STRONG', 'SPAN', 'B']);
     }
+  }
+  if (places.length) {
+    console.log(places);
+    replaceInTypes(places, replaceNames);
+  }
+
+  // Twitter is always global
+  if (items.twitter) {
+    replaceInType('*', replaceTwitter);
   }
 });
 
-function replaceInTypes(types, twitter) {
+function replaceInTypes(types, callback) {
   types.forEach( function(type) {
-    replaceInType(type, twitter);
+    replaceInType(type, callback);
   });
 }
 
-function replaceInType(type, twitter) {
+function replaceInType(type, callback) {
   var elements = document.getElementsByTagName(type);
 
   for (var i = 0; i < elements.length; i++) {
@@ -35,28 +47,39 @@ function replaceInType(type, twitter) {
 
       if (node.nodeType === 3) {
         var text = node.nodeValue;
-        var replacedText = text;
-        console.log(node);
-
-        // Set up tokens
-        if (twitter) {
-          replacedText = replacedText.replace(/realDonaldTrump/gi, '@realfloridaman__dt__twitter');
-        }
-        replacedText = replacedText.replace(/(president )*donald trump/gi, '@realfloridaman__dt');
-        replacedText = replacedText.replace(/(president )*Trump/g, '@realfloridaman__dt');
-        replacedText = replacedText.replace(/florida man/gi, '@realfloridaman__fm');
-
-        // Replace tokens
-        if (twitter) {
-          replacedText = replacedText.replace(/@realfloridaman__dt__twitter/gi, '_FloridaMan');
-        }
-        replacedText = replacedText.replace(/@realfloridaman__fm/gi, 'Donald Trump');
-        replacedText = replacedText.replace(/@realfloridaman__dt/gi, 'Florida Man');
-
+        var replacedText = callback(text);
         if (replacedText !== text) {
           element.replaceChild(document.createTextNode(replacedText), node);
         }
       }
+
     }
   }
+}
+
+function replaceNames(replacedText) {
+  // Set up tokens
+  replacedText = replacedText.replace(/DonaldTrump/gi, '@realfloridaman__dt__tight');
+  replacedText = replacedText.replace(/(president )*donald trump/gi, '@realfloridaman__dt');
+  replacedText = replacedText.replace(/(president )*Trump/g, '@realfloridaman__dt');
+  replacedText = replacedText.replace(/florida man/gi, '@realfloridaman__fm');
+
+  // Replace tokens
+  replacedText = replacedText.replace(/@realfloridaman__dt__tight/gi, 'FloridaMan');
+  replacedText = replacedText.replace(/@realfloridaman__fm/gi, 'Donald Trump');
+  replacedText = replacedText.replace(/@realfloridaman__dt/gi, 'Florida Man');
+
+  return replacedText;
+}
+
+function replaceTwitter(replacedText) {
+  // Set up tokens
+  replacedText = replacedText.replace(/realDonaldTrump/gi, '@realfloridaman__dt__twitter');
+  replacedText = replacedText.replace(/_FloridaMan/gi, '@realfloridaman__fm__twitter');
+
+  // Replace tokens
+  replacedText = replacedText.replace(/@realfloridaman__dt__twitter/gi, '_FloridaMan');
+  replacedText = replacedText.replace(/@realfloridaman__fm__twitter/gi, 'realDonaldTrump');
+
+  return replacedText;
 }
